e89fe17b7ab8f32f8a3c5460970b8f3a0778cce6
Stynos <stijn.db013@gmail.com>
2022-11-30 21:11:25 +0100

    Users can now upload posts to database

--- securesite/blog/views.py
+++ securesite/blog/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 def signup(request):
     
@@ -100,4 +102,18 @@ def settings(request):
         return redirect('settings')    
     return render(request, 'setting.html', {'user_profile': user_profile})
 
+@login_required(login_url='signin')
+def upload(request):
     
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        
+        return redirect('/')
+    else:
+        redirect('/')
+    return HttpResponse('<h1>Upload View</h1>')
