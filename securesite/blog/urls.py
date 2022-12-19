80563a5e6ebc9a2662a499365c024399a98b18ac
Stynos <stijn.db013@gmail.com>
2022-12-19 14:21:23 +0100

    register not working

--- securesite/blog/urls.py
+++ securesite/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
@@ -11,5 +11,7 @@ urlpatterns = [
     path('like-post', views.like_post, name='like-post'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('follow', views.follow, name='follow'),
-    path('search', views.search, name='search'),    
+    path('search', views.search, name='search'),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),    
 ]
